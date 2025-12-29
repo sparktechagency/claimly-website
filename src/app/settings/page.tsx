44459935302a0d546d0b1a750ff7f2b900337f72
ramohin1236 <mohinr26@gmail.com>
2025-12-29 12:22:36 +0600

    added

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import backIcon from "../../../public/back.svg";
 import lock from "../../../public/Group (12).svg";
 import cross from "../../../public/Frame (10).svg";
+import arrow from "../../../public/Vector 87.svg";
+import redarrow from "../../../public/Vector 87 (1).svg";
 
 const page = () => {
   return (
@@ -29,33 +31,57 @@ const page = () => {
 
           <div className="flex flex-col gap-4">
             {/* change pass */}
-            <div className="bg-linear-to-l from-[#DBEAFE]/20 to-[#DBEAFE] flex items-center gap-3 py-6 px-6 rounded-xl border-[#DBEAFE]">
+            <div className="bg-linear-to-l from-[#DBEAFE]/20 to-[#DBEAFE] flex items-center gap-3 py-6 px-6 rounded-xl border-[#DBEAFE] justify-between">
+              <div className="flex items-center gap-4">
+                <div>
+                  <Image
+                    src={lock}
+                    alt="lock"
+                    width={100}
+                    height={100}
+                    className="w-6"
+                  />
+                </div>
+                <div>
+                  <p className="text-xl text-[#64748B]">Change password </p>
+                </div>
+              </div>
+
               <div>
                 <Image
-                  src={lock}
-                  alt="lock"
-                  width={100}
-                  height={100}
-                  className="w-6"
+                  src={arrow}
+                  alt="back"
+                  width={12}
+                  height={12}
+                  className="w-3"
                 />
-              </div>
-              <div>
-                <p className="text-xl text-[#64748B]">Change password </p>
               </div>
             </div>
             {/* delete */}
-            <div className="bg-linear-to-l from-[#FBCFE8]/20 to-[#FBCFE8]/80 flex items-center gap-3 py-6 px-6 rounded-xl border-[#DBEAFE]">
+            <div className="bg-linear-to-l from-[#FBCFE8]/20 to-[#FBCFE8]/80 flex items-center gap-3 py-6 px-6 rounded-xl border-[#DBEAFE] justify-between">
+              <div className="flex items-center gap-4">
+                <div>
+                  <Image
+                    src={cross}
+                    alt="lock"
+                    width={100}
+                    height={100}
+                    className="w-6"
+                  />
+                </div>
+                <div>
+                  <p className="text-xl text-[#ef4444]">Delete account  </p>
+                </div>
+              </div>
+
               <div>
                 <Image
-                  src={cross}
-                  alt="lock"
-                  width={100}
-                  height={100}
-                  className="w-6"
+                  src={redarrow}
+                  alt="back"
+                  width={12}
+                  height={12}
+                  className="w-3"
                 />
-              </div>
-              <div>
-                <p className="text-xl text-[#64748B]">Change password </p>
               </div>
             </div>
           </div>
