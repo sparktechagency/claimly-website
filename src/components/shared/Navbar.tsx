18c4b048fa67e592a3682e6e290f7dd531e4d930
ramohin1236 <mohinr26@gmail.com>
2026-01-04 15:18:30 +0600

    with usr

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   // 🔑 temporary auth state (later replace with real auth)
-  const isLogin = false;
+  const isLogin = true;
 
 
   const pathname = usePathname();
