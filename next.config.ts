e0fc5c6a1f95a2e30e73b942c63e3d9bcff6c894
ramohin1236 <mohinr26@gmail.com>
2026-03-01 11:07:30 +0600

    protocol added

--- next.config.ts
+++ next.config.ts
@@ -8,14 +8,23 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "**.devtunnels.ms",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "claimly-insurance-server.vercel.app",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "d1828hp9binr8g.cloudfront.net",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "13.237.138.182",
+        port: "5000",
+        pathname: "/**",
       },
     ],
   },
